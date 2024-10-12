24435905fc4cf0b091837d95ae0fff3f3df16f09
siddharthatanwar <siddharthatanwar@gmail.com>
2024-10-12 17:47:24 +0530

    Add files via upload
    this file has all python basics loop[s condition statement explanation to understand the basics of python

---
+++ PythonBasics.py
@@ -0,0 +1,43 @@
+# Question 1 Describe the role  of predefined keywords in Python and provide examples of  how they are used in a program.
+
+# Answer - 1 Predefined keywords in Python Predefined keywords in Python are reserved words that have special meanings
+#  and serve specific purposes in the language. They cannot be used as identifiers (like variable names,
+#  function names, etc.) because they are fundamental to the syntax and functionality of the language.
+
+# Role of Predefined Keywords:
+# 1.	Control Flow: Keywords like if, else, elif, for, while, and break control the flow of execution in a program.
+# 2.	Data Types and Structures: Keywords such as class, def, return, and import help define data types, functions, and modules.
+# 3.	Exception Handling: Keywords like try, except, finally, and raise manage error handling.
+# 4.	Boolean Contexts: Keywords like True, False, and None represent fundamental Boolean values and a null value.
+
+# Examples of Predefined Keywords in a Program:
+# Here are some examples that illustrate how these keywords are used in a Python program:
+
+
+# 1. Control Flow:
+# python
+# Copy code
+
+age = 18       # here notice that age is a VARIABLE created and defined by user it's not predefined keyword
+if age >= 18:  # here, if is a Predefined Keyword
+    print("You are an adult.")
+else:                              # here else is a Predefined Keyword
+    print("You are a minor.")
+
+
+#2.	Looping:
+# python
+# Copy code
+
+for i in range(5):       # here, "for" and "in" all are Predefined Keywords
+    print(i)
+
+
+#3. Boolean Values:
+# python
+# Copy code
+
+is_raining = True        # here, True is a Predefined Keyword
+if is_raining:
+    print("Take an umbrella.")
+
